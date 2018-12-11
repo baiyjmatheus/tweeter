0a7827a36cc2299dc5317834bd155800b1928c82
Matheus Bai <baiyj.matheus@gmail.com>
2018-12-11 18:38:01 +0000

    Refactored new-tweet char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  $("section.new-tweet form textarea").keyup(function(e) {
+  $("section.new-tweet form textarea").keyup(function() {
+    // calculate characters counter
     var charLength = $(this).val().length;
     var counter = 140 - charLength;
-    $("span.counter").text(counter);
+    $(this).siblings("span.counter").text(counter);
+    // Negative value
+    if (counter < 0) {
+      $(this).siblings("span.counter").addClass("negative");
+    } else {
+      $(this).siblings("span.counter").removeClass("negative");
+    }
   });
 });
